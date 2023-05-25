c06666dfe708a75642fb672a0fd5c2ea0decbaed
seongjun.oh <tiger8708@gmail.com>
2023-05-25 22:49:56 +0900

    기본 route 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,21 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import './App.css';
+import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from 'react-router-dom';
+import Home from './pages/Home';
+import Login from './pages/auth/Login';
+import Register from './pages/auth/Register';
+import Header from './components/nav/Header';
 
-function App() {
-    const [count, setCount] = useState(0);
+const router = createBrowserRouter(
+    createRoutesFromElements(
+        <Route path="/" element={<Header />}>
+            <Route index element={<Home />} />
+            <Route path="login" element={<Login />} />
+            <Route path="register" element={<Register />} />
+        </Route>,
+    ),
+);
 
-    return (
-        <>
-            <div>
-                <a href="https://vitejs.dev" target="_blank">
-                    <img src={viteLogo} className="logo" alt="Vite logo" />
-                </a>
-                <a href="https://react.dev" target="_blank">
-                    <img src={reactLogo} className="logo react" alt="React logo" />
-                </a>
-            </div>
-            <h1>Vite + React</h1>
-            <div className="card">
-                <button onClick={() => setCount(count => count + 1)}>count is {count}</button>
-                <p>
-                    Edit <code>src/App.tsx</code> and save to test HMR
-                </p>
-            </div>
-            <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
-        </>
-    );
-}
+const App = () => {
+    return <RouterProvider router={router} />;
+};
 
 export default App;
